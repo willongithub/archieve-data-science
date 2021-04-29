18ed5ce6b64f318bd1222ec30220c0a6fd9ceb32
liam@windows <me@windows.com>
2021-04-29 22:15:40 +1000

    Th

--- dcp/dcp_asm2.R
+++ dcp/dcp_asm2.R
@@ -1,7 +1,7 @@
 #' ---
 #' title: "Data Capture and Preparations G (11520)"
 #' subtitle: "Assignment 2"
-#' author: ""
+#' author: "Data Nerds"
 #' date: ""
 #' output:
 #'   pdf_document:
@@ -74,17 +74,30 @@ new_page <- links %>%
     url_absolute("https://en.wikipedia.org")
 
 ## 6
+# initialize a list to store the two entries
 criteria_list <- vector("list", len = 2)
 names(criteria_list) <- c("Cultural", "Natural")
 
+# read the two lists one by one
 for (i in 1:2) {
     criteria_list[[i]] <- new_page %>%
+        # load the html from web page
         read_html() %>%
+
+        # load the list which is in ordered list element
         html_elements("ol") %>%
         .[i] %>%
+
+        # load the items in the list
         html_elements("li") %>%
+
+        # retrive the text
         html_text2() %>%
+
+        # get rid of the reference notation at the end
         str_replace_all(pattern = "\\[.*]$", replacement = "") %>%
+
+        # remove undesired characters
         str_replace_all(pattern = "\"", replacement = "") %>%
         as.data.frame()
 }
@@ -95,26 +108,24 @@ for (i in 1:2) {
 data_list <- data_list[[1]][, !names(data_list[[1]]) %in% c("Image", "Refs")]
 
 ## 2
-# generate regex for different scenarios
+# generate regex for different naming scenarios
 re_country <- regex("
-            # match country names with specific site locations
+            # match country names with detail locations
+            # start with [,] and end with lower case letter followed by digit
+            # with special cases considered
             ((?<=,\\s{0,2})[A-Z][^,]*[a-z](?=([,*]?\\s?\\d)|[.]))
 
-            # match country names without specific site locations
-            # |([A-Z][^,]*[a-z\\)](?=(\\[.{0,2}\\s?.?])?\\s?\\d))
-
-            |([A-Z][^,]*[a-z\\)](?=(\\[.{4}])?\\s?\\d))
-
-            # |((?<=Jer)[:print:]+(?=\\[.{4}]))
-            # match Jerusalem
+            # match sites with only county names
+            # start with upper case letter
+            |([A-Z][^,A-Z]+([a-z]|[A-Z)]{7})(?=(\\[.{1,4}])?\\s?\\d))
             ", comments = T)
 
-str_view(a, "(\\[.{4}])?\\s?\\d")
-
-data_list["Location"]
-
-data_list$Location %>%
+data_list["Location"] <- data_list$Location %>%
+    # extract country names with regex defined above
     str_extract(pattern = re_country) %>%
+
+    # site 32 covers two country
+    # remove the referance notation and add mark "&" to make it clear
     str_replace(pattern = "[*]", replacement = " &") %>%
     as.data.frame()
 
@@ -133,24 +144,32 @@ data_list["Criteria"] <- data_list$Criteria %>%
     as.data.frame()
 
 ## 4
+# correct the column name from table legends retrived before
 names(data_list)[4] <- table_legends[[4]][1]
 
+# generate regex to match area number in acres
 re_acres <- regex("(?<=[(]).*(?=[)])")
 
+# extract the desired number and put it back
 data_list["Area"] <- data_list$Area %>%
     str_extract(pattern = re_acres) %>%
     as.data.frame()
 
 ## 5
+# generate regex to match the last 4 digit
 re_endangered <- regex("[:digit:]{4}.$")
 
 data_list["Endangered"] <- data_list$Endangered %>%
+    # match the very last year number
     str_extract(pattern = re_endangered) %>%
+
+    # remove the dash after year number and store as numeric value
     str_replace_all(pattern = ".$", replacement = "") %>%
     as.numeric() %>%
     as.data.frame()
 
 ## 6
+# Year (WHS) and Endangered are numeric while others are in charecters
 glimpse(data_list)
 
 #' ##################### Part 3 #####################
@@ -159,17 +178,29 @@ length(which(data_list$Type == "Cultural"))
 length(which(data_list$Type == "Natural"))
 
 ## 2
-data_list$Area %>%
-    str_replace_all(pattern = ",", replacement = "") %>%
-    as.numeric() %>%
-    which.max() %>%
-    data_list$Name[.]
+# factor for unit conversion
+acre_to_m2 <- 4046.86
 
-data_list$Area %>%
+# extract and convert the area to numeric value in square meters
+area_list <- data_list$Area %>%
     str_replace_all(pattern = ",", replacement = "") %>%
-    as.numeric() %>%
+    as.numeric() * acre_to_m2
+
+# find the largest sites
+area_list %>%
+    which.max() %>%
+    cat(data_list$Name[.],
+        "-",
+        area_list[.],
+        "(square meter)")
+
+# find the smallest sites
+area_list %>%
     which.min() %>%
-    data_list$Name[.]
+    cat(data_list$Name[.],
+        "-",
+        area_list[.],
+        "(square meter)")
 
 ## 3
 plot <- hist(data_list[["Endangered"]],
@@ -178,7 +209,7 @@ plot <- hist(data_list[["Endangered"]],
      ylab = "frequency",
      ylim = range(0, 20),
      col = "sienna2")
-    
+# add legend for clear view
 text(plot$mids,
      plot$counts,
      labels = plot$counts,
@@ -189,6 +220,7 @@ data_list %>%
     group_by(Location) %>%
     summarize(site_count = n()) %>%
     arrange(desc(site_count))
+# Syria has the most which is 6 sites
 
 ## 5
 data_list <- data_list %>%
@@ -201,7 +233,7 @@ plot <- hist(data_list[["years_before_listed"]],
      ylab = "frequency",
      ylim = range(0, 20),
      col = "sienna2")
-    
+# add legend for clear view
 text(plot$mids,
      plot$counts,
      labels = plot$counts,
