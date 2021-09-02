8966eb8fb922513955b6cf37a7f8d886fe30278e
liam@sourcetree <me@sourcetree.com>
2021-09-02 22:41:27 +1000

    Th

--- pds/io_data.py
+++ pds/io_data.py
@@ -48,25 +48,52 @@ def read_multi_dim_data(filename):
     result = []
     try:
         with open(filename, 'r') as file:
-
+            # skip empty line at the beginning
             while True:
                 raw_line = file.readline()
                 if len(raw_line) > 1:
                     break
-            
+            count = 0
+            # read lines and skip empty lines in between
             while True:
                 items = raw_line.split(',')
-                print(len(items))
-                print(items[2])
-                result.append(tuple(items[0:4]))
+                line = line.replace('\n', '')
+                result.append(tuple([float(n) for n in items[:4]]))
+                count += 1
+                # print("add entry: #", count, end='\r')
                 raw_line = file.readline()
-                if len(raw_line) == 1:
+                while len(raw_line) == 1:
                     raw_line = file.readline()
-                    continue
+                # reach the end of file
                 if len(raw_line) == 0:
+                    # print("EOF")
                     break
-        print("File close status: ", file.closed)
+        print("close file status:", file.closed)
     except Exception as e:
         print(e.args[0])
-
     return result
+
+
+# Question 9
+def disp_point(canvas, sample_list, radius, color):
+  r = radius
+  scale = 100
+  shift = 100
+  for sample in sample_list:
+    x = sample[0]*scale + shift
+    y = sample[1]*scale + shift
+    canvas.create_oval(x - r, y - r, x + r, y + r, fill=color)
+
+def disp_label(tkinker, canvas, sample_list):
+    scale = 100
+    shift = 100
+    for i in range(3):
+        x = sample_list[i][0]
+        y = sample_list[i][1]
+        label = "#" + str(i + 1) + " (" + str(x) + ", " + str(y) + ")"
+        x = x*scale + shift
+        y = y*scale + shift
+        x2 = x - 50
+        y2 = y + 150
+        canvas.create_line(x, y, x2, y2)
+        lb = tkinker.Label(canvas, text=label, bg='gray').place(x=x2, y=y2)
--- pds/pds_w5.py
+++ pds/pds_w5.py
@@ -59,5 +59,25 @@ import sys
 reload(sys.modules["pds.io_data"])
 
 iris_list = io.read_multi_dim_data("pds/data/iris.data")
+print(iris_list)
 
 # Q9
+import tkinter
+
+# centre samples
+centre_1 = (5.1, 3.0, 1.1, 0.5)
+centre_2 = (4.4, 3.2, 2.8, 0.2)
+centre_3 = (5.7, 3.9, 3.9, 0.8)
+centre_list = [centre_1, centre_2, centre_3]
+
+window = tkinter.Tk()
+window.title("Data Viwer")
+canvas = tkinter.Canvas(window, height=800, width=1280)
+
+io.disp_point(canvas, iris_list, 5, color="#eb0000")
+io.disp_point(canvas, centre_list, 5, color="#000000")
+
+io.disp_label(tkinter, canvas, centre_list)
+
+canvas.pack()
+window.mainloop()
