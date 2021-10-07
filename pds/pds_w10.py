6e6b2024afd932c52b9b50b2deb317ddaa55795e
liam@macos <me@macos.com>
2021-10-07 15:25:42 +1100

    Th

--- pds/pds_w10.py
+++ pds/pds_w10.py
@@ -100,3 +100,59 @@ plt.show()
 
 # %%
 # Example 9
+for i in np.arange(0, 300, 50):
+    x, y = [], []
+    values = np.linspace(start=0, stop=2*np.pi, num=100)
+    for j in values:
+        r = np.cos(i*j)
+        x.append(r*np.cos(j))
+        y.append(r*np.sin(j))
+plt.plot(x, y, c='r')
+plt.title('r = cos(20*i)')
+plt.show()
+
+# %%
+# Example 10
+for i in range(10):
+    R = 10
+    k, l = np.random.random()*2
+    x, y = []*2
+    values = np.linspace(0, 200, 400)
+    for j in values:
+        xx = R*((1 - k)*np.cos(j) + l*k*np.cos((1 - k)*j/k))
+        x.append(xx)
+        yy = R*((1 - k)*np.sin(j) + l*k*np.sin((1 - k)*j/k))
+        y.append(yy)
+    plt.plot(x, y, c='r')
+    plt.title(f'k = {k}, l = {l}')
+    plt.show()
+
+# %%
+from sklearn import datasets
+
+# %%
+# Example 11
+iris = datasets.load_iris()
+X = iris.data[:, :2]
+Y = iris.target
+labels = iris.target_names
+
+plt.scatter(X[:, 0], X[:, 1], label='iris', c='r', marker='o', s=50)
+
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.title('Iris dataset')
+plt.legend()
+plt.show()
+
+# %%
+# Queation 5
+plt.scatter(X[:50, 0], X[:50, 1], label='iris', c='r', marker='o', s=50)
+plt.scatter(X[49:100, 0], X[49:100, 1], label='iris', c='g', marker='s', s=50)
+plt.scatter(X[99:150, 0], X[99:150, 1], label='iris', c='b', marker='^', s=50)
+
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.title('Iris dataset')
+plt.legend()
+plt.show()
