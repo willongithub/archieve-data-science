8f47e66cfd5fb4b0576128089ef19cf95502bd2d
liam@macos <me@macos.com>
2022-04-20 10:23:49 +1000

    cvia lab week 11 update

--- cvia/w11/Wk11_fasterRCNN_BirdCameraTrap_Subset.m
+++ cvia/w11/Wk11_fasterRCNN_BirdCameraTrap_Subset.m
@@ -13,27 +13,35 @@ close all;
 clear;
 % clc;
 
-%% Load the ground truth data
+%% 
+% Load the ground truth data
 load('WanyaEastSubset_GroundTruth.mat');
 
-%% Change file paths in the ground truth as the labelling was done on a different computer
+%% 
+% Change file paths in the ground truth as the labelling was done on a different computer
 alternativePaths = ["/Users/s423738/Dropbox/UC/Teaching/8890_CVIA_PG/2022/Data/Wanya_East_181115_to_091215_DM_Subset" "D:\Dev\archieve-data-science\cvia\assets\"];
+% alternativePaths = ["/Users/s423738/Dropbox/UC/Teaching/8890_CVIA_PG/2022/Data/Wanya_East_181115_to_091215_DM_Subset" "assets/"];
+
 unresolvedPaths = changeFilePaths(gTruth, alternativePaths);
 
-%% For the labelled ground truth data, get the image data store and bounding box information.
+%% 
+% For the labelled ground truth data, get the image data store and bounding box information.
 [imds, blds] = objectDetectorTrainingData(gTruth);
 
-%% Combine the two datastores
+%% 
+% Combine the two datastores
 cds = combine(imds, blds);
 
-%% Check if we have a GPU available and clear any old data from it
-disp('GPU count')
-disp(gpuDeviceCount())
-disp(gpuDevice(1))
-device = gpuDevice(1);
-reset(device);  % Clear previous values that might still be on the GPU
+%% 
+% Check if we have a GPU available and clear any old data from it
+% disp('GPU count')
+% disp(gpuDeviceCount())
+% disp(gpuDevice(1))
+% device = gpuDevice(1);
+% reset(device);  % Clear previous values that might still be on the GPU
 
-%% Set the training options
+%% 
+% Set the training options
 options = trainingOptions('sgdm', ...
         'InitialLearnRate', 0.001, ...
         'MiniBatchSize', 4, ...
@@ -42,7 +50,8 @@ options = trainingOptions('sgdm', ...
         'Shuffle', 'every-epoch', ...
         'VerboseFrequency', 1);
 
-%% Prepare the fasterRCNN training. To keep things simple, we use
+%% 
+% Prepare the fasterRCNN training. To keep things simple, we use
 %  alexnet as the network model architecture. Our data has 4 classes and we
 %  set the default image size to 227x227 pixels. These are RGB images.
 inputImageSize =  [227 227 3];
@@ -54,7 +63,7 @@ featureExtractionNetwork = layerGraph(alexnet('Weights', 'none'));
 featureLayer = 'relu5';
 
 preprocessedTrainingData = ...
-    transform(cds, @(data) preprocessData(data, inputImageSize)); 
+    transform(cds, @(da) preprocessData(da, inputImageSize)); 
 
 numAnchors = 3;
 anchorBoxes = estimateAnchorBoxes(preprocessedTrainingData, numAnchors);
@@ -62,29 +71,39 @@ anchorBoxes = estimateAnchorBoxes(preprocessedTrainingData, numAnchors);
 lgraph = fasterRCNNLayers(inputImageSize, numClasses, anchorBoxes, ...
                           featureExtractionNetwork, featureLayer);
 
-%% Train the fasterRCNN object detector model
+%% 
+% Train the fasterRCNN object detector model
 [detector, info] = ...
     trainFasterRCNNObjectDetector(preprocessedTrainingData, lgraph, options, ...
                     'FreezeBatchNormalization', false, ...
                     'NegativeOverlapRange',[0 0.3], ...
                     'PositiveOverlapRange',[0.6 1]);
 
-%% Now, let's test the object detector
+%% 
+% Now, let's test the object detector
 fname = imds.Files{40};     % Pick an image
+% fname = preprocessedTrainingData.UnderlyingDatastores{1, 1}.UnderlyingDatastores{1, 1}.Files{40};
 img = imread(fname);
 [bboxes, scores] = detect(detector, img);   % Detect objects
 
+% Check results
+whos bboxes scores
+
+% Show results
 if (~isempty(bboxes))   % If any known objects found, overlay bounding box
     img = insertObjectAnnotation(img, 'rectangle', bboxes, scores);
 end
-figure,imshow(img);     % Display image and detected objects
 
-%% Helper function - Preprocessing the training data to fit the target 
+figure
+imshow(img);     % Display image and detected objects
+
+%% 
+% Helper function - Preprocessing the training data to fit the target 
 %  image size. If we resize the input images, we must also resize the
 %  bounding box(es) that belong to the images.
 function data = preprocessData(data, targetSize)
-scale = targetSize(1:2)./size(data{1}, [1 2]);
-data{1} = imresize(data{1}, targetSize(1:2));
-bboxes = round(data{2});
-data{2} = bboxresize(bboxes, scale);
+    scale = targetSize(1:2)./size(data{1}, [1 2]);
+    data{1} = imresize(data{1}, targetSize(1:2));
+    bboxes = round(data{2});
+    data{2} = bboxresize(bboxes, scale);
 end
