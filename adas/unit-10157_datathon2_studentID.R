1446b1f9907e62a8e2eb9ed56b72973ff2accc4b
liam@windows <me@windows.com>
2022-04-29 23:58:18 +1000

    adas asm4 init

---
+++ adas/unit-10157_datathon2_studentID.R
@@ -0,0 +1,36 @@
+## Applied Data Analysis in Sport 10157
+## Datathon 2
+## <put your student ID here>
+
+
+## Question 21 code goes here
+
+
+
+
+
+## Question 22 code goes here
+
+
+
+
+
+## Question 23 code goes here
+
+
+
+
+
+## Question 24 code goes here
+
+
+
+
+
+## Question 25 code goes here
+
+
+
+
+
+## Question 26 code goes here
