d4615731edc3df0bc56dd681cd3120f708e4f0a9
liam@windows <me@windows.com>
2022-05-15 18:55:02 +1000

    cvia asm1 dl wrap

--- cvia/asm1/cvia_asm1.m
+++ cvia/asm1/cvia_asm1.m
@@ -7,100 +7,36 @@
 % path = "assets\CUB_200_2011\";
 path = "..\assets\CUB_200_2011_Subset20classes\";
 
-% % Read image classes and labels
-% classNames = readtable(fullfile(path, "classes.txt"), ... 
-%     "ReadVariableNames", false); 
-% classNames.Properties.VariableNames = ["index", "className"]; 
- 
-labels = readtable(fullfile(path, "image_class_labels.txt"), ... 
-    "ReadVariableNames", false); 
-labels.Properties.VariableNames = ["index", "label"]; 
-
 % Read image file paths
 images = readtable(fullfile(path, "images.txt"), ...  
     "ReadVariableNames", false); 
 images.Properties.VariableNames = ["index", "file"];
 
-% Read bounding box info
+% Read object bounding box info
 bboxes = readtable(path + "bounding_boxes.txt", ... 
     'ReadVariableNames', false);
 bboxes.Properties.VariableNames = {'index', 'x', 'y', 'w', 'h'};
+bm = mapBboxes(images(:, 2), bboxes(:, 2:5));
 
-imds = imageDatastore(fullfile(path, "images", images.file));
-imds = shuffle(imds);
-bbds = arrayDatastore(bboxes(:, 2:5));
-bbds = shuffle(bbds);
+% Target size of input images
+ts = [128, 128];
 
-data = combine(imds, bbds);
-data = transform(data, @cropImages);
+% Datastore of full images
+fids = imageDatastore(fullfile(path, "images", images.file), ...
+    "labelSource", "foldernames");
+fids.ReadFcn = @(file) readImages(file, ts);
 
-%%
-a = readall(data);
-%% 
-% Split dataset
-dataSize = height(dataFiles);
+% Datastore of cropped images (via bounding boxes)
+cids = imageDatastore(fullfile(path, "images", images.file), ...
+    "labelSource", "foldernames");
+cids.ReadFcn = @(file) readImages(file, ts, bm);
 
-rng default;  % for reproducibility
-indexAll = randperm(dataSize);  % shuffled index number
+% Split datasets
+[trainFull, validateFull, testFull] = splitEachLabel(fids, 0.6, 0.2);
+[trainCropped, validateCropped, testCropped] = splitEachLabel(cids, 0.6, 0.2);
 
-indexTrain = indexAll(1:floor(dataSize*0.6));
-indexValidate = indexAll(floor(dataSize*0.6) + 1:floor(dataSize*0.8));
-indexTest = indexAll(floor(dataSize*0.8) + 1:end);
-
-% Create image datastores
-trainingDS = imageDatastore(fullfile(path, "images", ... 
-    dataFiles(indexTrain, :).file), "labelSource", "foldernames", ... 
-    "FileExtensions", ".jpg"); 
-
-validationDS = imageDatastore(fullfile(path, "images", ... 
-    dataFiles(indexValidate, :).file), "labelSource", "foldernames", ... 
-    "FileExtensions", ".jpg"); 
-
-testDS = imageDatastore(fullfile(path, "images", ... 
-    dataFiles(indexTest, :).file), "labelSource", "foldernames", ... 
-    "FileExtensions", ".jpg"); 
-
-% % Combine datastores and labels
-% trainingCDS = combine(trainingDS, arrayDatastore(trainingDS.Labels));
-% validationCDS = combine(validationDS, arrayDatastore(validationDS.Labels));
-% testCDS = combine(testDS, arrayDatastore(testDS.Labels));
-
-% Combine datastores and bounding box
-trainingCDS = combine(trainingDS, arrayDatastore(dataFiles(indexTrain, 3:6)));
-validationCDS = combine(validationDS, arrayDatastore(dataFiles(indexValidate, 3:6)));
-testCDS = combine(testDS, arrayDatastore(dataFiles(indexTest, 3:6)));
-
-% Resize and crop input image
-% targetSize = [256, 256];
-targetSize = [128, 128];
-
-% Whole image
-trainingWholeCDS = transform(trainingCDS, ... 
-    @(x) preprocessData(x, targetSize)); 
-
-validationWholeCDS = transform(validationCDS, ... 
-    @(x) preprocessData(x, targetSize)); 
-
-testWholeCDS = transform(testCDS, ... 
-    @(x) preprocessData(x, targetSize));
-
-% Image within bounding box only
-% trainingBox = arrayDatastore(dataFiles(indexTrain, 3:6));
-trainingBoxCDS = transform(trainingCDS, trainingBox, @(img, box) ... 
-    preprocessData(img, targetSize, box)); 
-
-% validationBox = arrayDatastore(dataFiles(indexValidate, 3:6));
-validationBoxCDS = transform(validationCDS, validationBox, @(img, box) ... 
-    preprocessData(img, targetSize, box)); 
-
-% testBox = arrayDatastore(dataFiles(indexTest, 3:6));
-testBoxCDS = transform(testCDS, testBox, @(img, box) ... 
-    preprocessData(img, targetSize, box)).UnderlyingDatastores{1, 1}; 
-
-% Combine datastores and labels
-trainingWholeCDS = combine(trainingWholeCDS, arrayDatastore(trainingDS.Labels));
-validationWholeCDS = combine(validationWholeCDS, arrayDatastore(validationDS.Labels));
-testWholeCDS = combine(testWholeCDS, arrayDatastore(testDS.Labels));
+% Split for 5-fold validation
+[part1, part2, part3, part4, part5] = splitEachLabel(cids, 0.2, 0.2, 0.2, 0.2);
 
 %% Task 1: Classic Machine Learning Approach
 
@@ -113,7 +49,7 @@ end
 
 % Network layers
 layers = [
-    imageInputLayer([targetSize 3])
+    imageInputLayer([ts 3])
     
     convolution2dLayer(3, 8, 'Padding','same')
     batchNormalizationLayer
@@ -142,112 +78,163 @@ layers = [
     convolution2dLayer(3, 128, 'Padding', 'same')
     batchNormalizationLayer
     reluLayer
+
+    maxPooling2dLayer(2,'Stride',2)
     
-    fullyConnectedLayer(200)
+    convolution2dLayer(3, 256, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(20)
     softmaxLayer
     classificationLayer];
 
-%% Experiment 1
-% Training options
-options = trainingOptions('sgdm', ...
-    'InitialLearnRate', 0.001, ...
-    'MiniBatchSize', 20, ...
-    'MaxEpochs', 10, ...
-    'Shuffle', 'every-epoch', ...
-    'ValidationData', validationWholeCDS, ...
-    'VerboseFrequency', 1, ...
-    'Verbose', true, ...
-    'Plots', 'training-progress');
+%% Experiment 1 - Full Image with Classic Machine Learning
 
-% Train the model
-model = trainNetwork(trainingCDS, layers, options);
 
-% Display test results
-YPred = classify(model, testCDS);
-YTest = testDS.Labels;
+%% Experiment 2 - Full Image with Deep Learning
+% Combine datastores for the model
+trainFullDS = combine(trainFull, arrayDatastore(trainFull.Labels));
+validateFullDS = combine(validateFull, arrayDatastore(validateFull.Labels));
+testFullDS = combine(testFull, arrayDatastore(testFull.Labels));
 
-accuracy = sum(YPred == YTest)/numel(YTest)
+% Run deep learning training process
+runProcessDL(layers, trainFullDS, validateFullDS, testFullDS)
 
-% [m, order] = confusionmat(YTest, YPred);
-% figure("Name", "Test Set Result");
-% confusionchart(m, order, ...
-%     'ColumnSummary','column-normalized', ...
-%     'RowSummary','row-normalized');
-% title("Overall Accuracy: "+ string(round(accuracy*100, 1)) +"%");
+%% Experiment 3 - Cropped Images with Classic Machine Learning
 
-%% Experiment 2
-figure
-imshow(trainingWholeCDS.read{1});
-title("Resized Sample Image")
-%% Experiment 3
-% Training options
-options = trainingOptions('sgdm', ...
-    'InitialLearnRate', 0.001, ...
-    'MiniBatchSize', 20, ...
-    'MaxEpochs', 10, ...
-    'Shuffle', 'every-epoch', ...
-    'ValidationData', validationBoxCDS, ...
-    'VerboseFrequency', 1, ...
-    'Verbose', true, ...
-    'Plots', 'training-progress');
 
-% Train the model
-model = trainNetwork(trainingBoxCDS, layers, options);
+%% Experiment 4 - Cropped Images with Deep Learning
+% Combine datastores for the model
+trainCroppedDS = combine(trainCropped, arrayDatastore(trainCropped.Labels));
+validateCroppedDS = combine(validateCropped, arrayDatastore(validateCropped.Labels));
+testCroppedDS = combine(testCropped, arrayDatastore(testCropped.Labels));
 
-% Display test results
-YPred = classify(model, testBoxCDS);
-YTest = testDS.Labels;
-
-accuracy = sum(YPred == YTest)/numel(YTest)
-
-% [m, order] = confusionmat(YTest, YPred);
-% figure("Name", "Test Set Result");
-% confusionchart(m, order, ...
-%     'ColumnSummary','column-normalized', ...
-%     'RowSummary','row-normalized');
-% title("Overall Accuracy: "+ string(round(accuracy*100, 1)) +"%");
-
-%% Experiment 4
+% Run deep learning training process
+runProcessDL(layers, trainCroppedDS, validateCroppedDS, testCroppedDS)
 
 %% Experiment 5
+% All partitions
+parts = {part1, part2, part3, part4, part5};
+
+%%
+% Run 1: Parts 1-3 for training, Part 4 for validation, Part 5 for test 
+run1 = runProcessDL(layers, getCVData(1:3, 4, 5, parts, ts, bm));
+
+% Run 2: Parts 2-4 for training, 5 for validation, 1 for test
+run2 = runProcessDL(layers, getCVData(2:4, 5, 1, parts, ts, bm));
+
+% Run 3: Parts 3-5 for training, 1 for validation, 2 for test
+run3 = runProcessDL(layers, getCVData(3:5, 1, 2, parts, ts, bm));
+
+% Run 4: Parts 4, 5, and 1 for training, 2 for validation, 3 for test
+run4 = runProcessDL(layers, getCVData([4 5 1], 2, 3, parts, ts, bm));
+
+% Run 5: Parts 5, 1, and 2 for training, 3 for validation, 4 for test
+run5 = runProcessDL(layers, getCVData([5 1 2], 3, 4, parts, ts, bm));
+
+% Cross-validation result
+oa = mean([run1 run2 run3 run4 run5]);
+disp("Overall Accuracy (5-fold CV): "+ string(round(oa*100, 1)) +"%")
 
 %% Helper functions
-% function output = preprocessData(input, targetSize, crop)
-function output = preprocessData(input, targetSize, varargin)
-
-    if nargin == 3
-        x = varargin{1}(1, 1);
-        y = varargin{1}(1, 2);
-        w = varargin{1}(1, 3);
-        h = varargin{1}(1, 4);
-        output{1} = imcrop(input{1}, [x, y, w, h]);
-    end
-
-    output{1} = imresize(input{1}, targetSize(1:2));
-    output{2} = input{2};
-    
-    if size(input{1}, 3) < 3
-        output{1} = cat(3, output{1}, output{1}, output{1});
+%
+function map = mapBboxes(img, box)
+    map = containers.Map;
+    for i = 1:height(img)
+        key = split(img{i, "file"}, ["\", "/"]);
+        key = key{end};
+        map(key) = [box{i, :}];
     end
 end
 
-function out = cropImages(in)
-    x = table2array(in{2}(1, 1));
-    disp(in{2}(1, 1));
-    y = table2array(in{2}(1, 2));
-    w = table2array(in{2}(1, 3));
-    h = table2array(in{2}(1, 4));
+%
+function output = readImages(input, targetSize, varargin)
+    img = imread(input);
+    map = varargin{1};
 
-    if size(in{1}, 3) < 3
-        out = cat(3, in{1}, in{1}, in{1});
+    if nargin == 3
+        file = split(input, ["\", "/"]);
+        box = map(file{end});
+
+        if box(1) > size(img, 2) || box(2) > size(img, 1)
+            disp("error")
+        end
+
+        img = imcrop(img, [box(1), box(2), box(3), box(4)]);
+    end
+    
+    if size(img, 3) < 3
+        img = cat(3, img, img, img);
+    end
+        output = imresize(img, targetSize);
+end
+
+%
+function restult = runProcessDL(layers, varargin)
+    if nargin < 4
+        training = varargin{1}{1};
+        validation = varargin{1}{2};
+        testing = varargin{1}{3};
     else
-        out = in{1};
+        training = varargin{1};
+        validation = varargin{2};
+        testing = varargin{3};
     end
 
-    if x > size(out, 2) || y > size(out, 1)
-        disp("error")
-        disp([x, y, w, h])
-    else
-        out = imcrop(out, [x, y, w, h]);
-    end
+    % Training options
+    options = trainingOptions('sgdm', ...
+        'InitialLearnRate', 0.001, ...
+        'MiniBatchSize', 20, ...
+        'MaxEpochs', 10, ...
+        'Shuffle', 'every-epoch', ...
+        'ValidationData', validation, ...
+        'VerboseFrequency', 1, ...
+        'Verbose', false, ...
+        'Plots', 'training-progress');
+    
+    % Train the model
+    model = trainNetwork(training, layers, options);
+    
+    % Display test results
+    YPred = classify(model, testing);
+    YTest = testing.UnderlyingDatastores{1, 1}.Labels;
+    
+    accuracy = sum(YPred == YTest)/numel(YTest);
+    disp("Overall Accuracy: "+ string(round(accuracy*100, 1)) +"%")
+
+    restult = accuracy;
+end
+
+%
+function results = getCVData( ...
+    trainingPartition, validationPartition, testingPartition, parts, ts, bm)
+    % Combine training set
+    trds = imageDatastore(cat(1, ...
+        parts{trainingPartition(1)}.Files, ...
+        parts{trainingPartition(2)}.Files, ...
+        parts{trainingPartition(3)}.Files));
+    trds.ReadFcn = @(file) readImages(file, ts, bm);
+
+    trlbds = arrayDatastore(cat(1, ...
+        parts{trainingPartition(1)}.Labels, ...
+        parts{trainingPartition(2)}.Labels, ...
+        parts{trainingPartition(3)}.Labels));
+
+    training = combine(trds, trlbds);
+
+    % Combine validation set
+    vdds = imageDatastore(parts{validationPartition}.Files);
+    vdds.ReadFcn = @(file) readImages(file, ts, bm);
+    vdlbds = arrayDatastore(parts{validationPartition}.Labels);
+    valitation = combine(vdds, vdlbds);
+
+    % Combine testing set
+    tsds = imageDatastore(parts{testingPartition}.Files);
+    tsds.ReadFcn = @(file) readImages(file, ts, bm);
+    tsds.Labels = parts{testingPartition}.Labels;
+    tslbds = arrayDatastore(parts{testingPartition}.Labels);
+    testing = combine(tsds, tslbds);
+
+    results = {training, valitation, testing};
 end
